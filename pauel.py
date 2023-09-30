d84ace99bc6df142af50427e598f7a5c818aca2d
Mr1proper <pauel2200@mail.ru>
2023-10-01 01:28:23 +0300

    oo

---
+++ pauel.py
@@ -0,0 +1,16 @@
+# первое впечатление
+# hello_string ="я занимаюсь питоном:"
+# second_string="ddd"
+# age=1240
+# one=1
+# number=0.1222
+# w=one+number
+# print(+ str(len(hello_string + second_string)+ "ppp"))
+
+num=10
+# print(num-1)
+# print(num-2)
+# print(num-3)
+# print(num-4)
+for i in range(num):
+    print("я люблю "+str(i))
