ea3313028dabb100d1bfe241c957242c771a4b3c
Даниил Урасинов <urasinovjr@MacBook-Pro-Daniil.local>
2023-10-07 15:06:28 +0300

    oh no, coding

--- lessons/first/URUS/homeworkurus.py
+++ lessons/first/URUS/homeworkurus.py
@@ -1,43 +1,39 @@
-a = 4
-count = 0
-for i in range(a):
-    print(" " * (4-i-1) + '*' * (2*i+1))
-    count = count + (2*i+1)
-else:
-    print("  ***")
-    count = count + 3
-    print("Количество символов: " + str(count))
+# a = 4
+# for i in range(a):
+#     print(" " * (4-i-1) + '*' * (2*i+1))
+# for i in range(1,2):
+#     print("  ***")
+#     print("Количество символов: 19")
 
 
-for i in range(1,11):
-    if i % 2 == 0:
-        print(i)
+# for i in range(1,11):
+#     if i % 2 == 0:
+#         print(i)
 
 
-sum = 0
-for i in range(1,11):
-    if i % 3 == 0:
-        sum += i
-print(sum)
+# sum = 0
+# for i in range(1,11):
+#     if i % 3 == 0:
+#         sum += i
+# print(sum)
 
-name = "Программирование на Python"
-bukvy = "оаиеy"
-sum = 0
-for i in name:
-    if i in bukvy:
-        sum = sum + 1
-print(sum)
+# name = "Программирование на Python"
+# bukvy = "оаиеy"
+# sum = 0
+# for i in name:
+#     if i in bukvy:
+#         sum = sum + 1
+# print(sum)
 
-annual_income = float(input("Введи годовой доход: "))
-credit_history = int(input("Количество положительных историй: "))
-for i in range(1):
-    if annual_income >= 80000 and credit_history >= 2:
-        print("Высокий рейтинг")
-    elif annual_income >= 60000 and credit_history >= 1:
-        print("Средний рейтинг")
-    elif annual_income < 60000 and credit_history < 1:
-        print("Низкий рейтинг")
-    else:
-        print('Невозможно присвоить рейтинг')
+# annual_income = float(input("Введи годовой доход: "))
+# credit_history = int(input("Количество положительных историй: "))
+# for i in range(1):
+#     if annual_income >= float(80000) and credit_history >= 2:
+#         print("Высокий рейтинг")
+# for i in range(1):
+#     if annual_income >= float(60000) and credit_history >= 1:
+#         print("Средний рейтинг")
+# for i in range(1):
+#     if annual_income < float(60000) and credit_history < 1:
+#         print("Низкий рейтинг")
 
-# if, elif, else
